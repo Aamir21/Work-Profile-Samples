23980797a02a6d72cf375c3b03401fddfb03e209
Md. Aamir Sohail <42406623+Aamir21@users.noreply.github.com>
2020-11-02 14:18:37 +0530

    Add files via upload

---
+++ WP1.php
@@ -0,0 +1,197 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">    
+    <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet'>
+    <link href="https://fonts.googleapis.com/css?family=Anton|Varela+Round&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Abril+Fatface|Amatic+SC|Anton|Baloo+Bhai|Caveat|Cinzel|Dancing+Script|Josefin+Sans|Lexend+Exa|Long+Cang|Pacifico|Patua+One|Quicksand|Righteous|Stoke|Varela+Round&display=swap" rel="stylesheet">
+    
+    
+<meta charset="utg-8">
+<meta name="viewport" width="device-width , initial-scale=1">
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+<!-- Popper JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+        
+        
+        
+        
+        
+        
+        
+        
+        <style type="text/css">
+            
+            
+            .seg1{
+                height: 120px;
+                background-color: aliceblue;
+                background-image: url(goindLOGO.png);
+                background-size: 100%;
+                background-repeat: no-repeat;
+                
+                
+            }
+            .seg2{
+                background-color: aliceblue;
+            }
+        
+            .seg3{
+                background-color: aliceblue;
+            }
+        
+        
+            
+            #f1{
+                width: auto;
+                margin-top: 40px;
+                margin-left: 100px;
+                
+                
+                
+            }
+            
+            
+            #text1{
+                border: solid thin grey ;
+                border-radius: 3px;
+                font-family: Pacifico;
+            }
+            #text2{
+                border: solid thin grey ;
+                border-radius: 3px;
+                font-family: Pacifico;
+            }
+            #submit{
+                width: 130px;
+                border: solid thin crimson ;
+                border-radius: 14px;
+                padding-left: 2px;
+                margin-left: 10px;
+                background: linear-gradient(90deg, crimson, magenta);
+                background-size: 400px;
+                box-shadow: 0px 0px 2px grey;
+                font-family: bahnschrift;
+                
+            }
+            
+            
+            
+            
+            .r2seg1{
+                height: 420px;
+                background-color: aliceblue;
+                
+            }
+            
+            .r2seg1 div{
+                height: 180px;
+                background:linear-gradient(45deg, aliceblue, white);
+                margin-top: 10px;
+                margin-left:15px;
+                border: solid thin grey;
+                border-radius: 3px;
+                box-shadow: 0px 0px 2px grey;
+                float: left;
+            }
+            
+            .r2seg2{
+                background-color: aliceblue;
+                height: 420px;
+                
+            }
+            
+            #caption{
+                font-size: 85px;
+                font-family: Pacifico;
+                
+            }
+        
+        
+        </style>
+    
+    
+    
+    
+    
+    </head>
+
+    <body>
+        
+        <div class="container-fluid">
+        
+            <div class="row">
+            
+                <div class="col-md-3 seg1">
+                    
+                    
+                
+                
+                
+                </div>
+                <div class="col-md-3 seg2"></div>
+                
+                <div class="col-md-6 seg3">
+                
+                    <form method="post" action="#" id="f1">
+                    
+                        <input type="text" placeholder="Name" name="val1" id="text1">
+                        <input type="text" placeholder="E-mail" name="val2" id="text2">
+                        <input type="submit" value="SignIN" name="sub" id="submit">
+                    
+                    </form>
+                
+                </div>
+            
+            </div>
+            
+            <!----ROW2--->
+            
+            <div class="row">
+            
+                <div class="col-md-4 r2seg1">
+                    
+                    <div class="col-md-4"></div>
+                    <div class="col-md-4"></div>
+                    <div class="col-md-4"></div>
+                    <div class="col-md-4"></div>
+                
+                
+                </div>
+                
+                <div class="col-md-8 r2seg2">
+                    <p id="caption">#Fly above peak of World!</p>
+                
+                
+                </div>
+            
+            
+            
+            
+            </div>
+        
+        
+        
+        
+        </div>
+    
+    
+    
+    
+    
+    
+    </body>
+
+
+
+</html>
